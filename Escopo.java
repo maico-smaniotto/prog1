c625b54eeb3b635aac25a98cc13eeb5f2019d92f
maico-smaniotto <maicosmaniotto@yahoo.com.br>
2014-09-25 19:36:54 -0300

    - removido o método setComandos da classe "Escopo", alterado o tipo do atributo "comandos" de String para String[] e o tipo do parâmetro "variaveis" do construtor de Variavel[] para ArrayList<Variavel>

--- Escopo.java
+++ Escopo.java
@@ -1,17 +1,13 @@
 class Escopo {
-	private String comandos;
+	private String[] comandos;
 	private ArrayList<Variavel> vars;
 	
-	public void setComandos(String v) {
-		this.comandos = v;
-	}
-	
-	public Escopo(String[] comandos, Variavel[] variaveis]) {
+	public Escopo(String[] comandos, ArrayList<Variavel> variaveis) {
 		this.comandos = comandos;
 		
 		this.vars = new ArrayList<Variavel>(); // Funciona como um array, mas tem método add() para adicionar itens
-		for (int i = 0; i < variaveis.length; i++) {
-			this.vars.add(variaveis[i]);
+		for (int i = 0; i < variaveis.size(); i++) {
+			this.vars.add(variaveis.get(i));
 		}
 	}
 	
